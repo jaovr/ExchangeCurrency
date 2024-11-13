cfe4b7f3ca3d65d0860e3be07e56a405dc7681e6
Joao Victor Rocha <joaov_rocha1@hotmail.com>
2024-11-13 14:26:35 -0300

    delete unnecessary import library

--- exchange/src/main/java/com/coin/exchange/controller/ExpenseController.java
+++ exchange/src/main/java/com/coin/exchange/controller/ExpenseController.java
@@ -3,7 +3,6 @@ package com.coin.exchange.controller;
 import com.coin.exchange.model.Expense;
 import com.coin.exchange.service.ExpenseCalculatorService;
 import com.fasterxml.jackson.databind.JsonNode;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
